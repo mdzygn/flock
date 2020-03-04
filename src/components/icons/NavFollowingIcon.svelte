ff2a57616b969d4714917ce5b5107a9c65ecf06b
Matt <mattdhalford@gmail.com>
2020-03-04 18:58:13 +1300

    icon png

--- src/components/icons/NavFollowingIcon.svelte
+++ src/components/icons/NavFollowingIcon.svelte
@@ -1,15 +1,21 @@
 <script>
-    import NavIcon from "../../assets/svg/nav_following.html";
-    import NavIconActive from "../../assets/svg/nav_following_active.html";
+    // import NavIcon from "../../assets/svg/nav_following.html";
+    // import NavIconActive from "../../assets/svg/nav_following_active.html";
 
     export let active = false;
 </script>
 
-<style>
-</style>
-
 {#if active}
-    <NavIconActive />
+    <img src='icons/nav_following_active.png' alt='Following' />
+    <!-- <NavIconActive /> -->
 {:else}
-    <NavIcon />
+    <img src='icons/nav_following.png' alt='Following' />
+    <!-- <NavIcon /> -->
 {/if}
+
+<style>
+    img {
+        width: 46px;
+        height: 46px;
+    }
+</style>

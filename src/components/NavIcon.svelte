3d3909275f5d64091c1e814056554891231dd97e
Matt <mattdhalford@gmail.com>
2020-03-04 19:51:29 +1300

    restore static png load

--- src/components/NavIcon.svelte
+++ src/components/NavIcon.svelte
@@ -1,14 +1,11 @@
 <script>
-    import icon from "./assets/icons/nav_home_active.png";
-
     export let iconId = 'home';
 
     export let active = false;
 </script>
 
 {#if active}
-    <img src='{icon}' alt='Following' />
-    <!-- <img src='assets/icons/nav_{iconId}_active.png' alt='Following' /> -->
+    <img src='assets/icons/nav_{iconId}_active.png' alt='Following' />
 {:else}
     <img src='assets/icons/nav_{iconId}.png' alt='Following' />
 {/if}

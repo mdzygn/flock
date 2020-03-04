b7d6d779576796124e5a9b754019c375d5e5afee
Matt <mattdhalford@gmail.com>
2020-03-04 19:02:27 +1300

    center align document, avoid transform center on document

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,29 +5,43 @@
 	export let segment;
 </script>
 
-<appContent>
-	<HeaderBar {segment}/>
-	<Nav {segment}/>
+<appContainer>
+	<appContent>
+		<HeaderBar {segment}/>
+		<Nav {segment}/>
 
-	<main>
-		<slot></slot>
-	</main>
-</appContent>
+		<main>
+			<slot></slot>
+		</main>
+	</appContent>
+</appContainer>
 
 <style>
 	@media (min-width: 480px) {
-		appContent {
+		appContainer {
 			position: absolute;
+			height: 100%;
+			width: 100%;
+
+			display:flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		appContent {
+			position: relative;
+
+			/* position: absolute;
+
+			left: 50%;
+			top: 50%;
+			transform: translate(-50%,-50%); */
 
 			width: 100%;
 			height: 100%;
 
 			max-width: 375px;
 			max-height: 667px;
-
-			left: 50%;
-			top: 50%;
-			transform: translate(-50%,-50%);
 
 			border: 1px solid #e0e0e0;
         	box-shadow: rgba(8, 8, 8, 0.2) 0px 5px 30px -5px;

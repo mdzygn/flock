79d5c9ed6ffd129dbab2fb06f136afee6875f6ef
Matt <mattdhalford@gmail.com>
2020-03-09 14:45:01 +1300

    persistent appModel store

--- src/models/appModel.js
+++ src/models/appModel.js
@@ -1,5 +1,55 @@
 import { writable } from 'svelte/store';
+import { DEBUG } from '../config';
 
-export const viewMode = writable('discover');
-export const locationMode = writable('global');
-export const exploreZoomed = writable(false);
+const localStorage = typeof window !== 'undefined' ? window.localStorage : undefined;
+
+const appState = loadState();
+
+setDefaultState(appState, 'viewMode', 'discover');
+setDefaultState(appState, 'locationMode', 'global');
+setDefaultState(appState, 'exploreZoomed', false);
+
+export const viewMode = writable(appState.viewMode);
+export const locationMode = writable(appState.locationMode);
+export const exploreZoomed = writable(appState.exploreZoomed);
+
+viewMode.subscribe(val => {appState.viewMode = val; saveState()});
+locationMode.subscribe(val => {appState.locationMode = val; saveState()});
+exploreZoomed.subscribe(val => {appState.exploreZoomed = val; saveState()});
+
+function setDefaultState(model, key, defaultValue) {
+    if (model[key] === undefined) {
+        model[key] = defaultValue;
+    }
+}
+
+function saveState() {
+    if (!localStorage) {
+        return;
+    }
+
+    console.log('save appState', appState);
+
+    localStorage.setItem('appState', JSON.stringify(appState));
+}
+
+function loadState() {
+    let appState = {};
+    if (!localStorage) {
+        return appState;
+    }
+    const appStateString = localStorage.getItem('appState');
+
+    console.log('load appState', appStateString);
+
+    if (appStateString) {
+        try {
+            appState = JSON.parse(appStateString);
+        } catch (e) {
+            if (DEBUG) {
+                console.warn('error loading app state from ' + appStateString);
+            }
+        }
+    }
+    return appState;
+}

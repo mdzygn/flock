de43541b2160ce307ac6e0db08e3d4c1b252788a
Matt <mattdhalford@gmail.com>
2020-03-09 13:58:03 +1300

    toggle zoom in explore mode

---
+++ src/routes/discover/_components/ExploreZoomed.svelte
@@ -0,0 +1,70 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	import Hotspots from '../../../components/Hotspots.svelte';
+	import Proxy from '../../../components/Proxy.svelte';
+
+	import ScrollView from '../../../components/ScrollView.svelte';
+
+	let projectId = 'm62lsp2o';
+</script>
+
+<svelte:head>
+	<title>Flock</title>
+</svelte:head>
+
+<div class="content">
+	<div class="contentArea">
+		<Hotspots>
+			<!-- Zoom Out -->
+			<div on:click="{e => dispatch('exploreZoomOut')}" style="
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;">&nbsp;</div>
+
+			<!-- Select Project -->
+			<a href="projects/{projectId}" style="
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				width: 250px;
+				height: 250px;
+				border-radius: 999px;">&nbsp;</a>
+		</Hotspots>
+
+		<Proxy image="explore_zoomed" className="exploreZoomed" />
+	</div>
+</div>
+
+<style>
+	.content {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+
+		background-color: #0B0B0B;
+
+		overflow: hidden;
+	}
+
+	.content :global(.hotspotContainer) {
+		top: 0;
+		height: 100%;
+	}
+
+	.contentArea {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+
+		display:flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.contentArea :global(.exploreZoomed) {
+		margin-top: 55px;
+	}
+</style>

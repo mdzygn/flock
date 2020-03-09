39e508a679a31eb6acab7ee6b359b30459312816
Matt <mattdhalford@gmail.com>
2020-03-09 16:05:01 +1300

    load project with followed and owner states

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,8 +1,23 @@
 <script>
+	import { goto } from '@sapper/app';
+
 	import ScrollView from '../../components/ScrollView.svelte';
 	import Proxy from '../../components/Proxy.svelte';
 
+	import { following, owner } from '../../models/projectViewState.js';
+
 	let projectId = 'm62lsp2o';
+
+	function loadOwnedProject() {
+		$owner = true;
+		$following = false;
+		goto('projects/' + projectId);
+	}
+	function loadFollowedProject() {
+		$owner = false;
+		$following = true;
+		goto('projects/' + projectId);
+	}
 </script>
 
 <svelte:head>
@@ -11,16 +26,16 @@
 
 <ScrollView id="projects">
 	<Proxy image="projects">
-		<a href="projects/{projectId}/my" style="
+		<div on:click="{loadOwnedProject}" style="
 			left: 0;
 			width: 100%;
 			top: 81px;
-			height: 196px;">&nbsp;</a>
-		<a href="projects/{projectId}/following" style="
+			height: 196px;">&nbsp;</div>
+		<div on:click="{loadFollowedProject}" style="
 			left: 0px;
 			width: 100%;
 			top: 359px;
-			height: 527px;">&nbsp;</a>
+			height: 527px;">&nbsp;</div>
 		<a href="projects/create" style="
 			right: 10px;
 			top: 277px;

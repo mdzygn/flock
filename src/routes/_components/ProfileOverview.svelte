6555d6a44e67383792148fb70265137f73f813e0
Matt <mattdhalford@gmail.com>
2020-03-16 12:34:53 +1300

    profile elements

--- src/routes/_components/ProfileOverview.svelte
+++ src/routes/_components/ProfileOverview.svelte
@@ -10,9 +10,14 @@
 	$: requestedConnection = $viewedUser ? $viewedUser.requestedConnection : false;
     $: viewingOwnProfile = $viewedUser ? $viewedUser.isCurrentUser : false;
 
-	$: username = $viewedUser ? $viewedUser.username : '';
+    $: username = $viewedUser ? $viewedUser.username : '';
+
+    $: userFullName = $viewedUser ? $viewedUser.fullName : '';
+    $: userFirstName = $viewedUser ? $viewedUser.firstName : '';
+    $: userBio = $viewedUser ? $viewedUser.bio : '';
 
 	$: coverImage = 'content/users/' + username + '/cover.jpg';
+	$: profileImage = 'content/users/' + username + '/profile.jpg';
 
     $: proxyImage = viewingOwnProfile ? 'profile_overview_owner' : 'profile_overview';
 	$: proxyOverviewActionsImage = viewingOwnProfile ? 'profile_overview_owner_actions' : 'profile_overview_actions';
@@ -29,6 +34,16 @@
                 height: 39px;" />
         </Proxy>
         <img src="{coverImage}" class="coverImage" alt="cover image" />
+        <div class="profileOverviewHeader">
+            <div class="profileImage">
+                <img src="{profileImage}" alt="{userFirstName}" />
+            </div>
+            <div class="itemContent">
+                <div class="header">{userFullName}</div>
+                <div class="username">@{username}</div>
+                <div class="description">{@html userBio}</div>
+            </div>
+        </div>
     </div>
     <Proxy image="{proxyOverviewActionsImage}">
         {#if !viewingOwnProfile}
@@ -71,5 +86,59 @@
 
 	.coverImage {
 		width: 100%;
+	}
+
+    /* .profileOverviewHeader {
+
+    } */
+    .profileImage {
+        position: absolute;
+        left: 14px;
+        margin-top: -48px;
+
+        width: 108px;
+        height: 108px;
+
+        border-radius: 999px;
+        overflow: hidden;
+        border: 4px solid #ffffff;
+    }
+    .profileImage img {
+        width: 100%;
+    }
+
+    .itemContent {
+        padding-top: 70px;
+        padding-left: 23px;
+    }
+
+    .header {
+        font-size: 2.2rem;
+        padding-top: 3px;
+        padding-right: 136px;
+    	line-height: 3rem;
+	}
+
+    .username {
+        font-size: 1.5rem;
+        padding-right: 60px;
+        padding-bottom: 5px;
+        margin-top: -6px;
+        line-height: 3rem;
+
+        color: #999999;
+    }
+
+    .description {
+		padding: 10px;
+		padding-top: 12px;
+        padding-right: 50px;
+
+        font-size: 1.5rem;
+		line-height: 2rem;
+
+		margin-left: -10px;
+
+		color: #555555;
 	}
 </style>

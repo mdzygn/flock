0321743f472d3efb0b841e77ace3adf21df750b3
Matt <mattdhalford@gmail.com>
2020-03-16 20:16:43 +1300

    search bar

---
+++ src/routes/_components/SearchBar.svelte
@@ -0,0 +1,66 @@
+<script>
+    import SearchIcon from "../../assets/icons/search.png";
+
+    export let searchString = '';
+    export let placeholder = 'Search';
+
+    let input;
+
+    function selectInput() {
+        if (input) {
+            input.focus();
+        }
+    }
+</script>
+
+<div class="searchBar">
+    <div class="searchBarField">
+        <div class="searchButton" on:click="{selectInput}">
+            <div class="searchIcon" style="background-image: url({SearchIcon})" />
+        </div>
+        <input bind:this="{input}" class="searchFieldInput" type="text" value="{searchString}" placeholder="{placeholder}" />
+    </div>
+</div>
+
+<style>
+    .searchBar {
+        width: 100%;
+    }
+
+    .searchBarField {
+        position: relative;
+        margin: 15px 16px;
+
+        border-radius: 100px;
+        background-color: rgba(0, 0, 0, 0.05);
+    }
+
+    .searchButton {
+        position: absolute;
+        right: -3px;
+        top: -7px;
+
+        padding: 10px;
+
+        cursor: pointer;
+    }
+
+    .searchIcon {
+        width: 20px;
+        height: 20px;
+        background-size: cover;
+    }
+
+    .searchFieldInput {
+        border: none;
+        outline: none;
+        background: none;
+
+        width: 100%;
+        box-sizing: border-box;
+
+        font-size: 1.5rem;
+        padding: 6px 12px;
+        padding-right: 28px;
+    }
+</style>
--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -2,7 +2,9 @@
 	import ScrollView from '../../components/ScrollView.svelte';
 
 	import Proxy from '../../components/Proxy.svelte';
-    import Hotspot from '../../components/Hotspot.svelte';
+	import Hotspot from '../../components/Hotspot.svelte';
+
+	import SearchBar from './../_components/SearchBar.svelte';
 
 	import { newProject, loadProject } from '../../actions/appActions.js';
 </script>
@@ -11,25 +13,33 @@
 	<title>Flock</title>
 </svelte:head>
 
-<ScrollView id="projects">
-	<Proxy image="projects">
-		<Hotspot onClick="{e => loadProject('n4vh55hg')}" style="
-			left: 0;
-			width: 100%;
-			top: 81px;
-			height: 196px;" />
-		<Hotspot onClick="{e => loadProject('msl30nhg')}" style="
-			left: 0px;
-			width: 100%;
-			top: 359px;
-			height: 527px;" />
-		<Hotspot onClick="{newProject}" style="
-			right: 10px;
-			top: 277px;
-			width: 142px;
-			height: 46px;" />
-	</Proxy>
-</ScrollView>
+<div class="content">
+	<ScrollView id="projects">
+		<Proxy image="projects" className="proxyOverlay">
+			<Hotspot onClick="{e => loadProject('n4vh55hg')}" style="
+				left: 0;
+				width: 100%;
+				top: 81px;
+				height: 196px;" />
+			<Hotspot onClick="{e => loadProject('msl30nhg')}" style="
+				left: 0px;
+				width: 100%;
+				top: 359px;
+				height: 527px;" />
+			<Hotspot onClick="{newProject}" style="
+				right: 10px;
+				top: 277px;
+				width: 142px;
+				height: 46px;" />
+		</Proxy>
+
+		<SearchBar />
+	</ScrollView>
+</div>
 
 <style>
+	.content :global(.proxyOverlay) {
+		position: absolute;
+		opacity: 0.5;
+	}
 </style>

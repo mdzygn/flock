7644497ac08df48caa7ac9b33675267d81f4ed92
Matt <mattdhalford@gmail.com>
2020-03-28 13:00:22 +1300

    tidy api requests

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,8 @@
+const base = (process.env.NODE_ENV === 'development') ? 'http://localhost:3000' : '';//'https://flock-hub.herokuapp.com';
+const folder = 'api';
+
+export const API_PATH = base + '/' + folder;
+
 export const MONGODB_URL = 'mongodb://heroku_4q6g8lwt:jiookdgcmutmsisg2v4inq89it@ds223812.mlab.com:23812/heroku_4q6g8lwt';
 export const MONGODB_DB = 'heroku_4q6g8lwt';
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
-let base = (process.env.NODE_ENV === 'development') ? 'http://localhost:3000' : '';//'https://flock-hub.herokuapp.com';
-let folder = 'api';
+import { API_PATH } from './config';
 
 function send(path, options) {
 	const fetch = process.browser ? window.fetch : require('node-fetch').default;
@@ -14,7 +13,7 @@ function send(path, options) {
 	// TODO: use authorization token
 	// opts.headers['Authorization'] = `Token ${token}`;
 
-	return fetch(`${base}/${folder}/${path}`, opts)
+	return fetch(`${API_PATH}/${path}`, opts)
 		.then(r => r.text())
 		.then(json => {
 			try {
@@ -24,47 +23,6 @@ function send(path, options) {
 			}
 		});
 }
-
-// function send({ method, path, data, token }) {
-// 	const fetch = process.browser ? window.fetch : require('node-fetch').default;
-
-// 	const opts = { method, headers: {} };
-
-// 	if (data) {
-// 		opts.headers['Content-Type'] = 'application/json';
-// 		opts.body = JSON.stringify(data);
-// 	}
-
-// 	if (token) {
-// 		opts.headers['Authorization'] = `Token ${token}`;
-// 	}
-
-// 	return fetch(`${base}/${folder}/${path}`, opts)
-// 		.then(r => r.text())
-// 		.then(json => {
-// 			try {
-// 				return JSON.parse(json);
-// 			} catch (err) {
-// 				return json;
-// 			}
-// 		});
-// }
-
-// function get(path, token) {
-// 	return send({ method: 'GET', path, token });
-// }
-
-// function del(path, token) {
-// 	return send({ method: 'DELETE', path, token });
-// }
-
-// function post(path, data, token) {
-// 	return send({ method: 'POST', path, data, token });
-// }
-
-// function put(path, data, token) {
-// 	return send({ method: 'PUT', path, data, token });
-// }
 
 function getProjects(options) {
 	return send('getProjects', options);
@@ -79,11 +37,6 @@ function updateProject(options) {
 }
 
 const api = {
-	// get,
-	// del,
-	// post,
-	// put,
-
 	getProjects,
 	addProject,
 	updateProject,

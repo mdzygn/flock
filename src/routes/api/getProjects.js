99537aa7b91184dde136c99d0d9002385d10a632
Matt <mattdhalford@gmail.com>
2020-04-02 19:40:24 +1300

    return followTime on projects, add likes lookup

--- src/routes/api/getProjects.js
+++ src/routes/api/getProjects.js
@@ -6,14 +6,22 @@ export async function post(req, res, next) {
 	const options = req.body;
 
 	let followedProjectIds = null;
+	let likedProjectIds = null;
 
 	if (options && options.userId) {
 		followedProjectIds = {};
+		likedProjectIds = {};
 
 		const follows = await db.collection('follows').find({userId: options.userId}).toArray();
 		for (var followI = 0; followI < follows.length; followI++) {
-			followedProjectIds[follows[followI].projectId] = true;
+			followedProjectIds[follows[followI].projectId] = follows[followI];
 		}
+
+		const likes = await db.collection('likes').find({userId: options.userId}).toArray();
+		for (var likeI = 0; likeI < likes.length; likeI++) {
+			likedProjectIds[likes[likeI].projectId] = true;
+		}
+
 		// console.log('followedProjectIds', followedProjectIds);
 	}
 
@@ -22,11 +30,16 @@ export async function post(req, res, next) {
 	// const projects = []; // to test returning no projects
 
 	if (followedProjectIds && projects) {
-		let project;
+		let project, followItem;
 		for (var projectI = 0; projectI < projects.length; projectI++) {
 			project = projects[projectI];
-			project.following = !!followedProjectIds[project.id];
-			project.dynamicFollowing = project.following;
+			project.liked = !!likedProjectIds[project.id];
+
+			followItem = followedProjectIds[project.id];
+			project.following = !!followItem;
+			if (project.following) {
+				project.followTime = followItem.createdAt;
+			}
 		}
 	}
 

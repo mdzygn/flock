c2a42c50833b6052cbf3d28bb9460c56111478c5
Matt <mattdhalford@gmail.com>
2020-04-13 15:01:47 +1200

    merge library collections

--- scripts/compileLibrary/index.js
+++ scripts/compileLibrary/index.js
@@ -15,21 +15,40 @@ function handleFiles(err, files) {
 
     var jsonFilePattern=/\.[json]+$/i;
 
-    files.forEach((fileName) => {
-        if (fileName.match(jsonFilePattern)) {
-            const filePath = inputPath + fileName;
-            const file = fs.readFileSync(path.join(__dirname, filePath), 'utf-8');
+    let filePath, file, data, collectionTags, collectionItems, newTags;
 
-            const data = JSON.parse(file);
+    files.forEach((fileName) => {
+        if (fileName.match(jsonFilePattern) && fileName.indexOf('_') !== 0) {
+            filePath = inputPath + fileName;
+            file = fs.readFileSync(path.join(__dirname, filePath), 'utf-8');
+
+            data = JSON.parse(file);
             console.log('\x1b[32m', 'reading ' + fileName, '\x1b[0m');
-            data.forEach((item) => {
-              outputData.push(item);
-            });
+
+            collectionTags = data.tags || '';
+            collectionItems = data.items || [];
+
+            console.log('collectionTags: ' + collectionTags);
+            console.log('collectionItems: ' + collectionItems.length);
+
+            if (collectionItems) {
+              collectionItems.forEach((item) => {
+                newTags = item.tags || '';
+                if (collectionTags) {
+                  if (newTags) {
+                    newTags = collectionTags + ', ' + newTags;
+                  } else {
+                    newTags = collectionTags;
+                  }
+                }
+                item.tags = newTags;
+                outputData.push(item);
+              });
+            }
         }
     });
 
     const outputJson = JSON.stringify(outputData, null, 2);
-    console.log(outputJson);
     fs.writeFile(path.join(__dirname, outputPath + outputFile), outputJson, function(err) {
       if (err) {
         throw('File save error: '+ err);

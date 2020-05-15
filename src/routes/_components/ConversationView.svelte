6333f4751284e048ce12ce3741c0bfed30058a69
Matt <mattdhalford@gmail.com>
2020-05-15 21:12:17 +1200

    align messages to bottom of message view align loader correctly at top left ensure messages scrolls to bottom on load

--- src/routes/_components/ConversationView.svelte
+++ src/routes/_components/ConversationView.svelte
@@ -1,79 +1,38 @@
 <script>
-    import locale from '../../locale';
-
-    import config from '../../config';
-
-	import { get, writable } from 'svelte/store';
+    // import locale from '../../locale';
+    // import config from '../../config';
+	// import { get, writable } from 'svelte/store';
 
     import Proxy from '../../components/Proxy.svelte';
     import Hotspot from '../../components/Hotspot.svelte';
 
 	import ContentPanel from './../_components/ContentPanel.svelte';
     import MessageItem from './MessageItem.svelte';
-    import ContentLoader from './../_components/ContentLoader.svelte';
 
-	import {
-        newProject,
-        loadCurrentConversation,
-    } from '../../actions/appActions';
-
-    import {
-        userId,
-        conversationId,
-        conversation,
-    } from '../../models/appModel';
-
-    import {
-        loadingConversations,
-    } from '../../models/conversationsModel';
-
-    import { getMessages, loadingMessages } from '../../models/messagesModel';
-
-    $: isLoadingMessages = $loadingMessages && (!$messages || !$messages.length);
-    $: isLoadingConversation = $loadingConversations && (!$conversation || ($conversation.id !== $conversationId));
-
-	loadCurrentConversation();
-
-    let messages = writable([]);
-    $: { messages = getMessages({ userId: $userId }) };
+    export let messages;
 
     export let className = '';
 </script>
 
 <div class="messageList {className}">
-    {#if isLoadingMessages || isLoadingConversation}
-        <ContentLoader label="{locale.LOADING.MESSAGES}" />
-    {:else}
-        {#each $messages as message}
-            <MessageItem {message} />
-        {:else}
-            <div class="noMessages">
-                {locale.CONVERSATION.NO_MESSAGES}
-            </div>
-        {/each}
-    {/if}
+    {#each $messages as message}
+        <MessageItem {message} />
+    {/each}
 </div>
 
 <style>
     .messageList {
+        width: 100%;
+        padding-bottom: 40px;
         /* padding-top: 9px; */
         /* padding-top: 10px; */
         /* padding-top: 44px; */
-        padding-bottom: 40px;
         /* padding-top: 310px; */
     }
 
     .messageList :global(.panelContent) {
         margin-top: -6px;
         margin-bottom: -6px;
-    }
-
-    .noMessages {
-        padding: 0 20px;
-        font-size: 1.2rem;
-        line-height: 1.8rem;
-        padding-top: 10px;
-        padding-bottom: 20px;
     }
 
     .messageList :global(.contentLoader) {
--- src/routes/messages/[conversation].svelte
+++ src/routes/messages/[conversation].svelte
@@ -1,14 +1,43 @@
 <script>
+	import { writable } from 'svelte/store';
+
+    import locale from '../../locale';
+
 	import ScrollView from '../../components/ScrollView.svelte';
 
 	import Proxy from '../../components/Proxy.svelte';
     import Hotspot from '../../components/Hotspot.svelte';
 
-	import { conversation } from '../../models/appModel';
 	import { loadProfile } from '../../actions/appActions';
+
+    import ContentLoader from '../_components/ContentLoader.svelte';
 
 	import ConversationView from '../_components/ConversationView.svelte';
 	import MessageInput from '../_components/MessageInput.svelte';
+
+    import {
+        userId,
+        conversationId,
+        conversation,
+	} from '../../models/appModel';
+
+    import {
+        loadingConversations,
+    } from '../../models/conversationsModel';
+
+	import { getMessages, loadingMessages } from '../../models/messagesModel';
+
+	import {
+        loadCurrentConversation,
+    } from '../../actions/appActions';
+
+	loadCurrentConversation();
+
+    export let messages = writable([]);
+    $: { messages = getMessages({ userId: $userId }) };
+
+    $: isLoadingMessages = $loadingMessages && (!$messages || !$messages.length);
+    $: isLoadingConversation = $loadingConversations && (!$conversation || ($conversation.id !== $conversationId));
 
 	$: viewingGroupConversation = ($conversation && $conversation.isGroup) || false;
 	$: isNewConversation = ($conversation && $conversation.isNew) || false;
@@ -21,33 +50,42 @@
 </svelte:head>
 
 <div class="messagesView">
-	<ScrollView anchorToBottom="{true}" id="conversation">
-		<!-- <div class="proxyOverlay">
-			{#if isNewConversation}
-				<div class="content">
-					<Proxy image="message_new_message_profile" className="profileInfo" />
-				</div>
-			{:else}
-				<Proxy image="{proxyMessageViewImage}">
-					{#if viewingGroupConversation}
-						<Hotspot onClick="{e => loadProfile('bl20a8lm')}" style="
-							left: 6px;
-							top: 25px;
-							width: 50px;
-							height: 307px;" />
-					{:else}
-						<Hotspot onClick="{e => loadProfile('bl20a8lm')}" style="
-							left: 6px;
-							top: 25px;
-							width: 50px;
-							height: 691px;" />
-					{/if}
-				</Proxy>
-			{/if}
-		</div> -->
+    {#if isLoadingMessages || isLoadingConversation}
+        <ContentLoader label="{locale.LOADING.MESSAGES}" />
+    {:else if !$messages || !$messages.length}
+        <ContentLoader label="{locale.CONVERSATION.NO_MESSAGES}" />
+        <!-- <div class="noMessages">
+            {locale.CONVERSATION.NO_MESSAGES}
+        </div> -->
+    {:else}
+		<ScrollView anchorToBottom="{true}" id="conversation">
+			<!-- <div class="proxyOverlay">
+				{#if isNewConversation}
+					<div class="content">
+						<Proxy image="message_new_message_profile" className="profileInfo" />
+					</div>
+				{:else}
+					<Proxy image="{proxyMessageViewImage}">
+						{#if viewingGroupConversation}
+							<Hotspot onClick="{e => loadProfile('bl20a8lm')}" style="
+								left: 6px;
+								top: 25px;
+								width: 50px;
+								height: 307px;" />
+						{:else}
+							<Hotspot onClick="{e => loadProfile('bl20a8lm')}" style="
+								left: 6px;
+								top: 25px;
+								width: 50px;
+								height: 691px;" />
+						{/if}
+					</Proxy>
+				{/if}
+			</div> -->
 
-		<ConversationView />
-	</ScrollView>
+			<ConversationView {messages} />
+		</ScrollView>
+	{/if}
 </div>
 
 <MessageInput />
@@ -59,6 +97,18 @@
 		bottom: 54px;
 		width: 100%;
 	}
+	.messagesView :global(.scrollContent) {
+		display: flex;
+		align-items: flex-end;
+	}
+
+    /* .noMessages {
+        padding: 0 20px;
+        font-size: 1.2rem;
+        line-height: 1.8rem;
+        padding-top: 10px;
+        padding-bottom: 20px;
+    } */
 
 	/* .messagesView :global(.proxyOverlay) {
         position: absolute;

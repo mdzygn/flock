60824a3259bdb8706826920afc490f667b49d614
Matt <mattdhalford@gmail.com>
2020-05-24 14:04:39 +1200

    transaction setup

--- src/routes/api/getMessages.js
+++ src/routes/api/getMessages.js
@@ -102,6 +102,50 @@ export async function post(req, res, next) {
 		}
 	}
 
+	// const session = client.startSession();
+	// const transactionOptions = {
+	// 	readPreference: 'primary',
+	// 	readConcern: { level: 'local' },
+	// 	writeConcern: { w: 'majority' }
+	// };
+
+	// try {
+	// 	const transactionResults = await session.withTransaction(async () => {
+	// 		const usersUpdateResults = await usersCollection.updateOne(
+	// 			{ email: userEmail },
+	// 			{ $addToSet: { reservations: reservation } },
+	// 			{ session });
+	// 		console.log(`${usersUpdateResults.matchedCount} document(s) found in the users collection with the email address ${userEmail}.`);
+	// 		console.log(`${usersUpdateResults.modifiedCount} document(s) was/were updated to include the reservation.`);
+
+	// 		const isListingReservedResults = await listingsAndReviewsCollection.findOne(
+	// 			{ name: nameOfListing, datesReserved: { $in: reservationDates } },
+	// 			{ session });
+	// 		if (isListingReservedResults) {
+	// 			await session.abortTransaction();
+	// 			console.error("This listing is already reserved for at least one of the given dates. The reservation could not be created.");
+	// 			console.error("Any operations that already occurred as part of this transaction will be rolled back.");
+	// 			return;
+	// 		}
+	// 		const listingsAndReviewsUpdateResults = await listingsAndReviewsCollection.updateOne(
+	// 			{ name: nameOfListing },
+	// 			{ $addToSet: { datesReserved: { $each: reservationDates } } },
+	// 			{ session });
+	// 		console.log(`${listingsAndReviewsUpdateResults.matchedCount} document(s) found in the listingsAndReviews collection with the name ${nameOfListing}.`);
+	// 		console.log(`${listingsAndReviewsUpdateResults.modifiedCount} document(s) was/were updated to include the reservation dates.`);
+	// 	}, transactionOptions);
+
+	// 	if (transactionResults) {
+	// 		console.log("The reservation was successfully created.");
+	// 	} else {
+	// 		console.log("The transaction was intentionally aborted.");
+	// 	}
+	// } catch(e){
+	// 	console.log("The transaction was aborted due to an unexpected error: " + e);
+	// } finally {
+	// 	await session.endSession();
+	// }
+
 	// let loadedTime = null;
 	// if (messages && messages.length) {
 	// 	const messageIds = messages.map((message) => message.id);

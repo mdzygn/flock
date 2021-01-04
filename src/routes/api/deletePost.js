18143f00552ca8977903b451f2ff24c6164ef622
Matt H <mattdhalford@gmail.com>
2021-01-04 18:34:48 +1300

    reduce channel, thread, project and user post counts on remove post

--- src/routes/api/deletePost.js
+++ src/routes/api/deletePost.js
@@ -24,89 +24,51 @@ export async function post(req, res, next) {
 			//TODO: check allowed to modify post, isOwner, !archive, !deleted
 
 			const result = await db.collection('posts').updateOne({ id: postId }, { $set: details } );
+			if (result) {
+				let updateParentCountResult;
+				switch (curPost.type) {
+					case 'thread':
+						updateParentCountResult = await db.collection('channels').updateOne({ id: curPost.channelId }, {
+							$inc: { postCount: -1 },
+						});
+						break;
+					case 'threadPost':
+						updateParentCountResult = await db.collection('posts').updateOne({ id: curPost.threadId }, {
+							$inc: { postCount: -1 },
+						});
+						break;
+				}
+
+				if (updateParentCountResult) {
+					const userUpdateResult = await db.collection('users').updateOne({ id: curPost.userId }, { $inc: { postsCount: -1 } });
+
+					if (userUpdateResult) {
+						const projectUpdateResult = await db.collection('projects').updateOne({ id: curPost.projectId }, {
+							$inc: { postsCount: -1 }
+						});
+
+						if (projectUpdateResult) {
+							// TODO: remove any relevant notifications
+
+							response(res, {success: true});
+						} else {
+							errorResponse(res, {addedPost: true});
+						}
+					} else {
+						errorResponse(res, {addedPost: true});
+					}
+				} else {
+					errorResponse(res, {addedPost: true});
+				}
+			} else {
+				errorResponse(res, {});
+			}
 
 			// if (result) {
-			// 	let updateParentCountResult;
-			// 	switch (details.type) {
-			// 		case 'thread':
-			// 			updateParentCountResult = await db.collection('channels').updateOne({ id: details.channelId }, {
-			// 				$set: { lastActiveAt: details.lastActiveAt },
-			// 				$inc: { postCount: 1 },
-			// 			});
-			// 			break;
-			// 		case 'threadPost':
-			// 			updateParentCountResult = await db.collection('posts').updateOne({ id: details.threadId }, {
-			// 				$set: { lastActiveAt: details.lastActiveAt },
-			// 				$inc: { postCount: 1 },
-			// 			});
-			// 			break;
-			// 	}
-
-			// 	if (updateParentCountResult) {
-			// 		const userUpdateResult = await db.collection('users').updateOne({ id: details.userId }, { $inc: { postsCount: 1 } });
-
-			// 		if (userUpdateResult) {
-			// 			const projectUpdateResult = await db.collection('projects').updateOne({ id: details.projectId }, { $set: { lastActiveAt: details.lastActiveAt }, $inc: { postsCount: 1 } });
-
-			// 			if (projectUpdateResult) {
-			// 				const postDetails = {
-			// 					id: true,
-
-			// 					userId: true,
-			// 					channelId: true,
-			// 					projectId: true,
-			// 					threadId: true,
-
-			// 					type: true,
-
-			// 					title: true,
-			// 					message: true,
-			// 					image: true,
-			// 				};
-
-			// 				const notificationResult = await createNotification(db, {
-			// 					type: NotificationTypes.POST_ADDED,
-			// 					actors: [{id: details.userId}],
-
-			// 					projectId: details.projectId,
-
-			// 					postId: details.id,
-			// 					threadId: details.threadId,
-			// 					channelId: details.channelId,
-			// 				}, {
-			// 					postTitle: details.title,
-			// 				}, res, {addedPost: true});
-			// 				if (notificationResult === -1) {
-			// 					return;
-			// 				}
-
-			// 				if (config.DEBUG) {
-			// 					if (notificationResult) {
-			// 						response(res, {success: true});
-			// 					} else {
-			// 						errorResponse(res, {addedPost: true}, {errorMsg: 'error creating addPost notification', result: notificationResult});
-			// 					}
-			// 				} else {
-			// 					errorResponse(res, {addedPost: true});
-			// 				}
-			// 			} else {
-			// 				errorResponse(res, {addedPost: true});
-			// 			}
-			// 		} else {
-			// 			errorResponse(res, {addedPost: true});
-			// 		}
-			// 	} else {
-			// 		errorResponse(res, {addedPost: true});
-			// 	}
+			// 	response(res, {success: true});
 			// } else {
-			// 	errorResponse(res, {});
+			// 	response(res, {error: true});
 			// }
-
-			if (result) {
-				response(res, {success: true});
-			} else {
-				response(res, {error: true});
-			}
 		} else {
 			response(res, {error: true, invalidUser: true});
 		}

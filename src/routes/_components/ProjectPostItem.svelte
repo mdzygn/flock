99c90ee90cc1336304e8adc2d157d172848b5d9a
Matt H <mattdhalford@gmail.com>
2021-01-07 19:50:51 +1300

    add project icon on project posts

--- src/routes/_components/ProjectPostItem.svelte
+++ src/routes/_components/ProjectPostItem.svelte
@@ -42,6 +42,10 @@
         postType,
         getIsProjectTeamMember,
     } from '../../models/appModel';
+    
+    import {
+        getProjectHeaderImage,
+    } from '../../models/projectsModel';
 
     import {
         loadProfile,
@@ -80,6 +84,7 @@
     // $: username = ($postUser && $postUser.username && '@' + $postUser.username) || '';
 
     $: projectTitle = ($project && $project.title) || '';
+    $: thumbImage = ($project && getProjectHeaderImage($project, true)) || null;
 
     $: commentsLabel = 'comment' + ($post.postCount >= 1 ? 's' : '');
 
@@ -166,6 +171,9 @@
         <!-- <div class="userName" class:button="{userLoaded}">{@html userName}</div> -->
         <!-- <div class="username" class:button="{userLoaded}">{username}</div> -->
     </div>
+    {#if thumbImage}
+        <img class="thumb" src="{thumbImage}" alt="{projectTitle}" />
+    {/if}
     <!-- <AvatarIcon user="{postUser}" onClick="{userLoaded ? viewUserProfile : null}" useThumb="{true}" /> -->
     <div class="postContent">
         <!-- <div class="date">{@html dateString}{#if showEdited}<span class="edited" title="{editedDate}">{locale.POST.EDITED}</span>{/if}</div> -->
@@ -277,6 +285,18 @@
         cursor: pointer;
     } */
 
+    .thumb {
+        width: 30px;
+        height: 30px;
+        position: absolute;
+        margin-top: 23px;
+        margin-left: 23px;
+
+        object-fit: cover;
+
+        border: 1px solid #D9D9D9;
+    }
+
     /* .threadPost :global(.avatarIcon) {
         position: absolute;
         left: 23px;
@@ -287,14 +307,10 @@
 
     .info {
         position: absolute;
-        padding-top: 18px;
-        /* padding-top: 24px; */
-        padding-right: 100px;
 
-        /* padding-top: 29px;
+        padding-top: 21px;
+        padding-left: 68px;
         padding-right: 100px;
-        padding-left: 104px; */
-        padding-left: 23px;
     }
 
     .projectTitle {
